f04bd36ff1fb0749051b5579e5fbad7c82741adf
musabkhan12 <awaisasif.9850@gmail.com>
2023-06-07 15:39:28 +0530

    Navbar changes responsive navbar  in file navbartest

--- src/components/layouts/Navbartest.js
+++ src/components/layouts/Navbartest.js
@@ -2,33 +2,43 @@ import React from "react"
 import "./Navbartest.css"
 import {Link} from "react-router-dom";
 function Navbartest(){
+    const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+if(hamburger){
+    hamburger.addEventListener("click", mobileMenu);
+}
+
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
    return(
        <div >
-            <meta name="viewport" 
-      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
-           <nav>
-    
-            <label >This is Testing</label>
-
-   
-     <ul>
-                <li>
+         <header class="header">
+        <nav class="navbar">
+            <a href="#" class="nav-logo">Learning Jar</a>
+            <ul class="nav-menu">
+                <li class="nav-item">
                 <Link to='/'>Home</Link>
                 </li>
-            </ul>
-            <ul>
-                <li>
-                    <Link to='/aboutus'>About Us</Link>
+                <li class="nav-item">
+                <Link to='/contactus'>Contact Us</Link>
                 </li>
-            </ul>
-            <ul>
-                <li>
-                    <Link to='/contactus'>Contact Us</Link>
+                <li class="nav-item">
+                    {/* <a href="/aboutus" class="nav-link">About</a> */}
+                    <Link to='/aboutus'>About</Link>
                 </li>
+             
             </ul>
-    
-        
-           </nav>
+            <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </nav>
+</header>
+
        </div>
    
    )
