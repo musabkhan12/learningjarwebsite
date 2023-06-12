06deea10234e388b459927a318c889b15fc4f0df
musabkhan12 <awaisasif.9850@gmail.com>
2023-06-12 11:54:00 +0530

    create a new page home 1 with hover effect on three sections student parent teacher

--- src/components/layouts/Home1.js
+++ src/components/layouts/Home1.js
@@ -34,6 +34,8 @@ function Home1() {
 
   return (
     <div class="main">
+          <meta name="viewport" 
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
   <Navbartest></Navbartest>
         <div class="three">
           <div class="boxtest">
