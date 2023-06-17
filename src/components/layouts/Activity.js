76fff6c6bda1629fb89f61aaf7e90c1829a0a77c
musabkhan12 <awaisasif.9850@gmail.com>
2023-06-17 15:33:28 +0530

    changes did with board text for laptop and also remove all small icons aimation

--- src/components/layouts/Activity.js
+++ src/components/layouts/Activity.js
@@ -1,40 +1,132 @@
 import React from "react";
 // import { useState } from "react";
 // import { Link } from "react-router-dom";
-import university from '../images/university.png'
-import bag from '../images/briefcase.svg'
-import envelope from '../images/envelope.svg'
-import briefcase from '../images/briefcase.svg'
-import flask from '../images/flask.svg'
-import like from '../images/like.svg'
-import worldwide from '../images/worldwide.svg'
-import loudspeaker from '../images/loudspeaker.svg'
-import quantum from '../images/quantum.svg'
-import targetarrow from '../images/targetarrow.svg'
-import rocket from '../images/rocket.svg'
+import university from "../images/university.png";
+import bag from "../images/briefcase.svg";
+import envelope from "../images/envelope.svg";
+import briefcase from "../images/briefcase.svg";
+import flask from "../images/flask.svg";
+import like from "../images/like.svg";
+import worldwide from "../images/worldwide.svg";
+import loudspeaker from "../images/loudspeaker.svg";
+import quantum from "../images/quantum.svg";
+import targetarrow from "../images/targetarrow.svg";
+import rocket from "../images/rocket.svg";
 
-import Navbartest from './Navbartest'
-import boardimg from "../images/board.svg"
- import Lottie from 'lottie-react';
+import Navbartest from "./Navbartest";
+import boardimg from "../images/board.svg";
+import Lottie from "lottie-react";
 // import anim from '../animation/animation.json'
 // import anim1 from '../animation/animation1.json'
 // import anim2 from '../animation/animation2.json'
 // import anim3 from '../animation/animation3.json'
 // import anim4 from '../animation/animation4.json'
 // import anim5 from '../animation/animation5.json'
- import anim6 from '../animation/animation6.json'
+import anim6 from "../animation/animation6.json";
 // import anim7 from '../animation/animationor.json'
 // import satisfaction from '../animation/satisfaction.json'
 import "./Activity.css";
 // import { LottiePlayer } from "lottie-react";
 function Activity() {
+  // from here features start
+  window.addEventListener("scroll", reveal);
+  function reveal() {
+    var reveal = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveal.length; i++) {
+      var windowweight = window.innerHeight;
+      var revealtop = reveal[i].getBoundingClientRect().top;
+      var revelpoint = 120;
+      if (revealtop < windowweight - revelpoint) {
+        reveal[i].classList.add("active");
+      } else {
+        reveal[i].classList.remove("active");
+      }
+    }
+  }
+  window.addEventListener("scroll", reveal1);
+  function reveal1() {
+    var reveal = document.querySelectorAll(".reveal1");
+    for (var i = 0; i < reveal.length; i++) {
+      var windowweight = window.innerHeight;
+      var revealtop = reveal[i].getBoundingClientRect().top;
+      var revelpoint = 120;
+      if (revealtop < windowweight - revelpoint) {
+        reveal[i].classList.add("active");
+      } else {
+        reveal[i].classList.remove("active");
+      }
+    }
+  }
+  //  here js for features end  //
 
   return (
     <div class="main">
-          <meta name="viewport" 
-      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
-   <Navbartest></Navbartest> 
-   {/* <div class="board">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+      ></meta>
+      <Navbartest></Navbartest>
+      <div class="boardlaptop">
+        <div class="boardlaptop1">
+          <Lottie class="anim6" animationData={anim6} />
+        </div>
+        <div class="boardlaptop2">
+          <div class="text2">
+          <div class="perspective-text2">
+          <div class="perspective-line2">
+            <h1 class="p2"></h1>
+            <h1 class="p2">Education</h1>
+          </div>
+          <div class="perspective-line2">
+            <h1 class="p2">Education</h1>
+            <h1 class="p2">without</h1>
+          </div>
+          <div class="perspective-line2">
+            <h1 class="p2">without</h1>
+            <h1 class="p2">application</h1>
+          </div>
+          <div class="perspective-line2">
+            <h1 class="p2">application</h1>
+            <h1 class="p2">is just</h1>
+          </div>
+          <div class="perspective-line2">
+            <h1 class="p2">is just</h1>
+            <h1 class="p2">entertainment</h1>
+          </div>
+          <div class="perspective-line2">
+            <h1 class="p2">entertainment</h1>
+            <h1 class="p2"></h1>
+          </div>
+        </div>
+          </div>
+          <div class="text1">
+          <div class="perspective-text">
+          <div class="perspective-line">
+            <p></p>
+            <p>TESTING </p>
+          </div>
+          <div class="perspective-line">
+            <p>TESTING</p>
+            <p>DATA</p>
+          </div>
+          <div class="perspective-line">
+            <p>DATA</p>
+            <p>PRACTICE</p>
+          </div>
+          <div class="perspective-line">
+            <p> PRACTICE</p>
+            <p>LEARN......</p>
+          </div>
+          <div class="perspective-line">
+            <p>LEARN......</p>
+            <p></p>
+          </div>
+        </div>
+          </div>
+        </div>
+
+      </div>
+      {/* <div class="board">
     <div class="boardtext">
     <div class="perspective-text">
   <div class="perspective-line">
@@ -57,142 +149,137 @@ function Activity() {
     <p>LEARN........</p>
     <p></p>
   </div>
-  
- 
-
-
     </div>
     </div>
    </div> */}
-     
-     <div class= "boardtext">
-      <img class ="envelope"  src={envelope} alt="" />
-      <img class ="briefcase"  src={briefcase} alt="" />
-      <img class ="flask"  src={flask} alt="" />
-      <img class ="likehand"  src={like} alt="" />
-      <img class ="worldwide"  src={worldwide} alt="" />
-      <img class ="loudspeaker"  src={loudspeaker} alt="" />
-      <img class ="quantum"  src={quantum} alt="" />
-      <img class ="targetarrow"  src={targetarrow} alt="" />
-      <img class ="rocket"  src={rocket} alt="" />
-      <img class="boardimg" src={boardimg} alt="" />
-   
-     <div class="perspective-text">
 
-  <div class="perspective-line">
+      <div class="boardtext">
+        {/* <img class="envelope" src={envelope} alt="" />
+        <img class="briefcase" src={briefcase} alt="" />
+        <img class="flask" src={flask} alt="" />
+        <img class="likehand" src={like} alt="" />
+        <img class="worldwide" src={worldwide} alt="" />
+        <img class="loudspeaker" src={loudspeaker} alt="" />
+        <img class="quantum" src={quantum} alt="" />
+        <img class="targetarrow" src={targetarrow} alt="" />
+        <img class="rocket" src={rocket} alt="" /> */}
+        <img class="boardimg" src={boardimg} alt="" />
 
-    <p></p>
-    <p>TESTING </p>
-  </div>
-  <div class="perspective-line">
-    <p>TESTING</p>
-    <p>DATA</p>
-  </div>
-  <div class="perspective-line">
-    <p>DATA</p>
-    <p>PRACTICE</p>
-  </div>
-  <div class="perspective-line">
-    <p> PRACTICE</p>
-    <p>LEARN......</p>
-  </div>
-  <div class="perspective-line">
-    <p>LEARN......</p>
-    <p></p>
-  </div>
-   </div>
-   <div class="perspective-text2">
-   <div class="perspective-line2">
-    <h1 class="p2" ></h1>
-    <h1 class="p2">Education</h1>
-  </div>
-   <div class="perspective-line2">
-    <h1 class="p2">Education</h1>
-    <h1 class="p2">without</h1>
-  </div>
-   <div class="perspective-line2">
-    <h1 class="p2">without</h1>
-    <h1 class="p2">application</h1>
-  </div>
-   <div class="perspective-line2">
-    <h1 class="p2">application</h1>
-    <h1 class="p2">is just</h1>
-  </div>
-  <div class="perspective-line2">
-    <h1 class="p2">is just</h1>
-    <h1 class="p2">entertainment</h1>
-  </div>
-   <div class="perspective-line2">
-    <h1 class="p2">entertainment</h1>
-    <h1 class="p2"></h1>
-  </div>
-
-   </div>
-     </div>
-     <div>
-     <div class="featuresdivmain">
-
-<h1 class="featuresheader reveal1">Features</h1>
-        <div class="featuresdiv reveal">
-          <div class="featuresubdivfirst1">
-            {/* <div>
+        <div class="perspective-text">
+          <div class="perspective-line">
+            <p></p>
+            <p>TESTING </p>
+          </div>
+          <div class="perspective-line">
+            <p>TESTING</p>
+            <p>DATA</p>
+          </div>
+          <div class="perspective-line">
+            <p>DATA</p>
+            <p>PRACTICE</p>
+          </div>
+          <div class="perspective-line">
+            <p> PRACTICE</p>
+            <p>LEARN......</p>
+          </div>
+          <div class="perspective-line">
+            <p>LEARN......</p>
+            <p></p>
+          </div>
+        </div>
+        <div class="perspective-text2">
+          <div class="perspective-line2">
+            <h1 class="p2"></h1>
+            <h1 class="p2">Education</h1>
+          </div>
+          <div class="perspective-line2">
+            <h1 class="p2">Education</h1>
+            <h1 class="p2">without</h1>
+          </div>
+          <div class="perspective-line2">
+            <h1 class="p2">without</h1>
+            <h1 class="p2">application</h1>
+          </div>
+          <div class="perspective-line2">
+            <h1 class="p2">application</h1>
+            <h1 class="p2">is just</h1>
+          </div>
+          <div class="perspective-line2">
+            <h1 class="p2">is just</h1>
+            <h1 class="p2">entertainment</h1>
+          </div>
+          <div class="perspective-line2">
+            <h1 class="p2">entertainment</h1>
+            <h1 class="p2"></h1>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="featuresdivmain">
+          <h1 class="featuresheader reveal1">Features</h1>
+          <div class="featuresdiv reveal">
+            <div class="featuresubdivfirst1">
+              {/* <div>
               <img class="featureimage" src={feature2} alt="" />{" "}
             </div> */}
-            <div class="cardcontent">
-              <h2 class="contentheader"> Teachers </h2>
-              <h4 class="contenttext">
-                {" "}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </h4>
+              <div class="cardcontent">
+                <h2 class="contentheader"> Teachers </h2>
+                <h4 class="contenttext">
+                  {" "}
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </h4>
+              </div>
             </div>
-          </div>
 
-          <div class="featuresubdivmiddle1 middlefeature">
-            {/* <div>
+            <div class="featuresubdivmiddle1 middlefeature">
+              {/* <div>
               <img class="featureimage" src={feature1} alt="" />
             </div> */}
-            <div class="cardcontent">
-              <h2 class="contentheader"> Teachers </h2>
-              <h4 class="contenttext">
-                {" "}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </h4>
+              <div class="cardcontent">
+                <h2 class="contentheader"> Teachers </h2>
+                <h4 class="contenttext">
+                  {" "}
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </h4>
+              </div>
             </div>
-          </div>
-          <div class="featuresubdivlast1">
-            {/* <div>
+            <div class="featuresubdivlast1">
+              {/* <div>
               <img class="featureimage" src={feature3} alt="" />
             </div> */}
-            <div class="cardcontent">
-              <h2 class="contentheader"> Teachers </h2>
-              <h4 class="contenttext">
-                {" "}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </h4>
+              <div class="cardcontent">
+                <h2 class="contentheader"> Teachers </h2>
+                <h4 class="contenttext">
+                  {" "}
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </h4>
+              </div>
             </div>
           </div>
         </div>
- </div>
-     </div>
-   </div>
+      </div>
+    </div>
   );
 }
 
